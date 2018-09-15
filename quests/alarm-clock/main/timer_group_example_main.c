4d20a5d6b2a961438d1d9c7c1f80e9f70224ff2a
Ellen Lo <lowing@bu.edu>
2018-09-15 00:10:52 -0400

    quest 1 first commit

--- quests/alarm-clock/main/timer_group_example_main.c
+++ quests/alarm-clock/main/timer_group_example_main.c
@@ -1,13 +1,15 @@
-/* Timer group-hardware timer example
+/*
+  First, set up console input for rate of alarm.
+    Re-initialize timer when there is valid input.
+    Perhaps 2 timers to switch ? ----- skip!
+  Second, control LEDs via GPIO to show alarm going off. ----- OK!
+  Third, connect servos to count down through alarming period.
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
+  Question: do I need to give user access to change the rate of alarming period?
 */
 
 #include <stdio.h>
+#include <string.h>
 #include "esp_types.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -15,18 +17,29 @@
 #include "soc/timer_group_struct.h"
 #include "driver/periph_ctrl.h"
 #include "driver/timer.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_log.h"
+#include "esp_attr.h"
+
+#include "driver/mcpwm.h"
+#include "soc/mcpwm_reg.h"
+#include "soc/mcpwm_struct.h"
 
 #define TIMER_DIVIDER         16  //  Hardware timer clock divider
 #define TIMER_SCALE           (TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
-#define TIMER_INTERVAL0_SEC   (3.4179) // sample test interval for the first timer
-#define TIMER_INTERVAL1_SEC   (5.78)   // sample test interval for the second timer
+#define TIMER_INTERVAL0_SEC   (60) // sample test interval for the first timer
 #define TEST_WITHOUT_RELOAD   0        // testing will be done without auto reload
 #define TEST_WITH_RELOAD      1        // testing will be done with auto reload
 
-/*
- * A sample structure to pass events
- * from the timer interrupt handler to the main program.
- */
+#define BLINK_GPIO 12
+
+#define SECONDHAND_GPIO 15
+#define SERVO_MIN_PULSEWIDTH 1000 //Minimum pulse width in microsecond
+#define SERVO_MAX_PULSEWIDTH 2000 //Maximum pulse width in microsecond
+#define SERVO_MAX_DEGREE 90 //Maximum angle in degree upto which servo can rotate
+
+
 typedef struct {
     int type;  // the type of timer's event
     int timer_group;
@@ -35,6 +48,32 @@ typedef struct {
 } timer_event_t;
 
 xQueueHandle timer_queue;
+
+static void mcpwm_example_gpio_initialize()
+{
+    printf("initializing mcpwm servo control gpio......\n");
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, SECONDHAND_GPIO);    //Set GPIO 15 as PWM0A, to which servo is connected
+    //mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, 32);
+}
+
+static uint32_t servo_per_degree_init(uint32_t degree_of_rotation)
+{
+    uint32_t cal_pulsewidth = 0;
+    cal_pulsewidth = (SERVO_MIN_PULSEWIDTH + (((SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) * (degree_of_rotation)) / (SERVO_MAX_DEGREE)));
+    return cal_pulsewidth;
+}
+
+void mcpwm_example_servo_control(uint32_t count)
+{
+    uint32_t angle;
+    uint32_t mapped_count = count * 90 / 60;
+    angle = servo_per_degree_init(mapped_count);
+    printf("pulse width: %dus\n", angle);
+    mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, angle);
+    //mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, angle);
+    //vTaskDelay(1000 / portTICK_PERIOD_MS);     //Add delay, since it takes time for servo to rotate, generally 100ms/60degree rotation at 5V
+    vTaskDelay(10);
+}
 
 /*
  * A simple helper function to print the raw timer counter value
@@ -63,7 +102,7 @@ void IRAM_ATTR timer_group0_isr(void *para)
        from the timer that reported the interrupt */
     uint32_t intr_status = TIMERG0.int_st_timers.val;
     TIMERG0.hw_timer[timer_idx].update = 1;
-    uint64_t timer_counter_value = 
+    uint64_t timer_counter_value =
         ((uint64_t) TIMERG0.hw_timer[timer_idx].cnt_high) << 32
         | TIMERG0.hw_timer[timer_idx].cnt_low;
 
@@ -104,7 +143,7 @@ void IRAM_ATTR timer_group0_isr(void *para)
  * auto_reload - should the timer auto reload on alarm?
  * timer_interval_sec - the interval of alarm to set
  */
-static void example_tg0_timer_init(int timer_idx, 
+static void example_tg0_timer_init(int timer_idx,
     bool auto_reload, double timer_interval_sec)
 {
     /* Select and initialize basic parameters of the timer */
@@ -124,51 +163,74 @@ static void example_tg0_timer_init(int timer_idx,
     /* Configure the alarm value and the interrupt on alarm. */
     timer_set_alarm_value(TIMER_GROUP_0, timer_idx, timer_interval_sec * TIMER_SCALE);
     timer_enable_intr(TIMER_GROUP_0, timer_idx);
-    timer_isr_register(TIMER_GROUP_0, timer_idx, timer_group0_isr, 
+    timer_isr_register(TIMER_GROUP_0, timer_idx, timer_group0_isr,
         (void *) timer_idx, ESP_INTR_FLAG_IRAM, NULL);
 
     timer_start(TIMER_GROUP_0, timer_idx);
 }
 
-/*
- * The main task of this example program
- */
-static void timer_example_evt_task(void *arg)
+void blink_task()
 {
-    while (1) {
-        timer_event_t evt;
-        xQueueReceive(timer_queue, &evt, portMAX_DELAY);
-
-        /* Print information that the timer reported an event */
-        if (evt.type == TEST_WITHOUT_RELOAD) {
-            printf("\n    Example timer without reload\n");
-        } else if (evt.type == TEST_WITH_RELOAD) {
-            printf("\n    Example timer with auto reload\n");
-        } else {
-            printf("\n    UNKNOWN EVENT TYPE\n");
+    int count = 0;
+    while(1) {
+        /* Blink off (output low) */
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        /* Blink on (output high) */
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        if (count < 2) {
+          count ++;
         }
-        printf("Group[%d], timer[%d] alarm event\n", evt.timer_group, evt.timer_idx);
-
-        /* Print the timer values passed by event */
-        printf("------- EVENT TIME --------\n");
-        print_timer_counter(evt.timer_counter_value);
-
-        /* Print the timer values as visible by this task */
-        printf("-------- TASK TIME --------\n");
-        uint64_t task_counter_value;
-        timer_get_counter_value(evt.timer_group, evt.timer_idx, &task_counter_value);
-        print_timer_counter(task_counter_value);
+        else {
+          gpio_set_level(BLINK_GPIO, 0);
+          break;
+        }
     }
 }
 
-/*
- * In this example, we will test hardware timer0 and timer1 of timer group0.
- */
+
+
 void app_main()
 {
-    timer_queue = xQueueCreate(10, sizeof(timer_event_t));
-    example_tg0_timer_init(TIMER_0, TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
-    example_tg0_timer_init(TIMER_1, TEST_WITH_RELOAD,    TIMER_INTERVAL1_SEC);
-    xTaskCreate(timer_example_evt_task, "timer_evt_task", 2048, NULL, 5, NULL);
-}
+    gpio_pad_select_gpio(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
+
+    //1. mcpwm gpio initialization
+    mcpwm_example_gpio_initialize();
+    //2. initial mcpwm configuration
+    printf("Configuring Initial Parameters of mcpwm......\n");
+    mcpwm_config_t pwm_config;
+    pwm_config.frequency = 50;    //frequency = 50Hz, i.e. for every servo motor time period should be 20ms
+    pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
+    pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);    //Configure PWM0A & PWM0B with above settings
+
+    timer_queue = xQueueCreate(10, sizeof(timer_event_t));
+    example_tg0_timer_init(TIMER_0, TEST_WITH_RELOAD, TIMER_INTERVAL0_SEC);
+    example_tg0_timer_init(TIMER_1, TEST_WITH_RELOAD, 1); // 2nd timer for counting seconds
+    while (1) {
+        uint32_t count = 0;
+        while(1){
+          timer_event_t evt_1;
+          xQueueReceive(timer_queue, &evt_1, portMAX_DELAY);
+          printf("second passing\n");
+          if(count >= TIMER_INTERVAL0_SEC){
+            mcpwm_example_servo_control(0);
+            break;
+          } else {
+            mcpwm_example_servo_control(count);
+            count ++;
+          }
+        }
+        //mcpwm_example_servo_control();
+        timer_event_t evt_0;
+        xQueueReceive(timer_queue, &evt_0, portMAX_DELAY);
+        // add 2nd timer and 2nd while loop to keep track of second ticking
+
+        blink_task();
+    }
+}
