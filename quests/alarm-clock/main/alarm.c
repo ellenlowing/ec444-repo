f9b82ef0b5edd2813ac7e6df1e570d73c294974b
Ellen Lo <lowing@bu.edu>
2018-09-23 04:27:23 -0400

    report for quest 1

--- quests/alarm-clock/main/alarm.c
+++ quests/alarm-clock/main/alarm.c
@@ -26,10 +26,10 @@
 #include "esp_vfs_fat.h"
 #include "nvs.h"
 #include "nvs_flash.h"
-
 #include "driver/mcpwm.h"
 #include "soc/mcpwm_reg.h"
 #include "soc/mcpwm_struct.h"
+#include "alpha.h"
 
 #define TIMER_DIVIDER         16  //  Hardware timer clock divider
 #define TIMER_SCALE           (TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
@@ -42,14 +42,11 @@
 
 #define SECONDHAND_GPIO 15
 #define MINUTEHAND_GPIO 33
-// #define START_SECOND 59
-// #define START_MINUTE 35
-// #define START_HOUR 12
 #define SECONDHAND_SERVO MCPWM_OPR_A
 #define MINUTEHAND_SERVO MCPWM_OPR_B
-#define SERVO_MIN_PULSEWIDTH 500 //Minimum pulse width in microsecond
-#define SERVO_MAX_PULSEWIDTH 2400 //Maximum pulse width in microsecond
-#define SERVO_MAX_DEGREE 90 //Maximum angle in degree upto which servo can rotate
+#define SERVO_MIN_PULSEWIDTH 500 
+#define SERVO_MAX_PULSEWIDTH 2400
+#define SERVO_MAX_DEGREE 180 
 
 #define BTN_GPIO 34
 
@@ -57,65 +54,7 @@
 #define RW_TEST_LENGTH                     129              /*!<Data length for r/w test, any value from 0-DATA_LENGTH*/
 #define DELAY_TIME_BETWEEN_ITEMS_MS        1000             /*!< delay time between different test items */
 
-#define I2C_EXAMPLE_SLAVE_SCL_IO           26               /*!<gpio number for i2c slave clock  */
-#define I2C_EXAMPLE_SLAVE_SDA_IO           27               /*!<gpio number for i2c slave data */
-#define I2C_EXAMPLE_SLAVE_NUM              I2C_NUM_0        /*!<I2C port number for slave dev */
-#define I2C_EXAMPLE_SLAVE_TX_BUF_LEN       (2*DATA_LENGTH)  /*!<I2C slave tx buffer size */
-#define I2C_EXAMPLE_SLAVE_RX_BUF_LEN       (2*DATA_LENGTH)  /*!<I2C slave rx buffer size */
-
-#define I2C_EXAMPLE_MASTER_SCL_IO          22               /*!< gpio number for I2C master clock */
-#define I2C_EXAMPLE_MASTER_SDA_IO          23               /*!< gpio number for I2C master data  */
-#define I2C_EXAMPLE_MASTER_NUM             I2C_NUM_1        /*!< I2C port number for master dev */
-#define I2C_EXAMPLE_MASTER_TX_BUF_DISABLE  0                /*!< I2C master do not need buffer */
-#define I2C_EXAMPLE_MASTER_RX_BUF_DISABLE  0                /*!< I2C master do not need buffer */
-#define I2C_EXAMPLE_MASTER_FREQ_HZ         100000           /*!< I2C master clock frequency */
-
-#define ESP_SLAVE_ADDR                     0x70             /*!< ESP32 slave address, you can set any 7bit value */
-#define WRITE_BIT                          I2C_MASTER_WRITE /*!< I2C master write */
-#define READ_BIT                           I2C_MASTER_READ  /*!< I2C master read */
-#define ACK_CHECK_EN                       0x1              /*!< I2C master will check ack from slave*/
-#define ACK_CHECK_DIS                      0x0              /*!< I2C master will not check ack from slave */
-#define ACK_VAL                            0x0              /*!< I2C ack value */
-#define NACK_VAL                           0x1              /*!< I2C nack value */
-
-#define LED_ON 1
-#define LED_OFF 0
-
-#define LED_RED 1
-#define LED_YELLOW 2
-#define LED_GREEN 3
-
-#define HT16K33_BLINK_CMD 0x80
-#define HT16K33_BLINK_DISPLAYON 0x01
-#define HT16K33_BLINK_OFF 0
-#define HT16K33_BLINK_2HZ  1
-#define HT16K33_BLINK_1HZ  2
-#define HT16K33_BLINK_HALFHZ  3
-
-#define HT16K33_CMD_BRIGHTNESS 0xE0
-
-static const uint8_t numbertable[] = {
-	0x3F, /* 0 */
-	0x06, /* 1 */
-	0xDB, /* 2 */
-	0xCF, /* 3 */
-	0xE6, /* 4 */
-	0xED, /* 5 */
-	0xFD, /* 6 */
-	0x07, /* 7 */
-	0xFF, /* 8 */
-	0xEF, /* 9 */
-	0xF7, /* a */
-	0xFC, /* b */
-	0x39, /* C */
-	0xDE, /* d */
-	0xF9, /* E */
-	0xF1, /* F */
-};
-
-SemaphoreHandle_t print_mux = NULL;
-uint8_t* data_wr;
-
+/******* GLOBAL VARIABLES*/
 volatile static bool reset = 0;
 const int CONNECTED_BIT = BIT0;
 
@@ -133,6 +72,9 @@ uint32_t second_count;
 uint32_t minute_count;
 uint32_t hour_count;
 
+
+
+/******* CONSOLE FUNCTIONS*/
 #if CONFIG_STORE_HISTORY
 
 #define MOUNT_PATH "/data"
@@ -247,12 +189,18 @@ void register_read()
     ESP_ERROR_CHECK( esp_console_cmd_register(&join_cmd) );
 }
 
+
+
+/******* PUSHBUTTON FUNCTIONS*/
 void IRAM_ATTR button_intr(void* arg) {
     if (!gpio_get_level(BTN_GPIO)) {
       reset = 1;
     }
 }
 
+
+
+/******* LED FUNCTIONS*/
 void blink_task()
 {
     printf("alarm going off\n");
@@ -272,6 +220,9 @@ void blink_task()
     }
 }
 
+
+
+/******* SERVO FUNCTIONS*/
 static void mcpwm_example_gpio_initialize()
 {
     printf("initializing mcpwm servo control gpio......\n");
@@ -296,13 +247,9 @@ void mcpwm_example_servo_control(uint32_t count,  mcpwm_operator_t operator)
     //vTaskDelay(10);
 }
 
-static void inline print_timer_counter(uint64_t counter_value)
-{
-    printf("Counter: 0x%08x%08x\n", (uint32_t) (counter_value >> 32),
-                                    (uint32_t) (counter_value));
-    printf("Time   : %.8f s\n", (double) counter_value / TIMER_SCALE);
-}
 
+
+/******* TIMER FUNCTIONS*/
 void IRAM_ATTR timer_group0_isr(void *para)
 {
     int timer_idx = (int) para;
@@ -439,226 +386,92 @@ static void example_tg1_timer_init(int timer_idx,
 }
 
 
-static void i2c_example_master_init()
-{
-    int i2c_master_port = I2C_EXAMPLE_MASTER_NUM;
-    i2c_config_t conf;
-    conf.mode = I2C_MODE_MASTER;
-    conf.sda_io_num = I2C_EXAMPLE_MASTER_SDA_IO;
-    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-    conf.scl_io_num = I2C_EXAMPLE_MASTER_SCL_IO;
-    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-    conf.master.clk_speed = I2C_EXAMPLE_MASTER_FREQ_HZ;
-    i2c_param_config(i2c_master_port, &conf);
-    i2c_driver_install(i2c_master_port, conf.mode,
-                       I2C_EXAMPLE_MASTER_RX_BUF_DISABLE,
-                       I2C_EXAMPLE_MASTER_TX_BUF_DISABLE, 0);
+
+/******* ALPHA-DISPLAY FUNCTIONS*/
+void setTime() {
+  writeDigit(0, hour_count / 10);
+  writeDigit(1, hour_count % 10);
+  writeDigit(2, minute_count / 10);
+  writeDigit(3, minute_count % 10);
 }
 
-static void i2c_example_slave_init()
-{
-    int i2c_slave_port = I2C_EXAMPLE_SLAVE_NUM;
-    i2c_config_t conf_slave;
-    conf_slave.sda_io_num = I2C_EXAMPLE_SLAVE_SDA_IO;
-    conf_slave.sda_pullup_en = GPIO_PULLUP_ENABLE;
-    conf_slave.scl_io_num = I2C_EXAMPLE_SLAVE_SCL_IO;
-    conf_slave.scl_pullup_en = GPIO_PULLUP_ENABLE;
-    conf_slave.mode = I2C_MODE_SLAVE;
-    conf_slave.slave.addr_10bit_en = 0;
-    conf_slave.slave.slave_addr = ESP_SLAVE_ADDR;
-    i2c_param_config(i2c_slave_port, &conf_slave);
-    i2c_driver_install(i2c_slave_port, conf_slave.mode,
-                       I2C_EXAMPLE_SLAVE_RX_BUF_LEN,
-                       I2C_EXAMPLE_SLAVE_TX_BUF_LEN, 0);
-}
 
-static void disp_buf(uint8_t* buf, int len)
-{
-    int i;
-    for (i = 0; i < len; i++) {
-        printf("%02x ", buf[i]);
-        if (( i + 1 ) % 16 == 0) {
-            printf("\n");
+
+/******* HELPER FUNCTION (CALL EVERY SEC)*/
+void ticking () {
+    timer_event_t evt_1;
+    xQueueReceive(timer_queue_1, &evt_1, portMAX_DELAY);
+    if(second_count > 58) {
+        second_count = 0;
+        if (minute_count > 58) {
+            minute_count = 0;
+            if (hour_count > 22) hour_count = 0;
+            else hour_count ++;
+        } else {
+            minute_count ++;
         }
+        mcpwm_example_servo_control(0, SECONDHAND_SERVO);
+        mcpwm_example_servo_control(minute_count, MINUTEHAND_SERVO);
+        setTime();
+    } else {
+        mcpwm_example_servo_control(second_count, SECONDHAND_SERVO);
+        second_count ++;
     }
-    printf("\n");
-}
-
-static esp_err_t blinkRate(i2c_port_t i2c_num, uint8_t b)
-{
-  if(b > 3) b = 0;
-  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-  i2c_master_start(cmd);
-  i2c_master_write_byte(cmd, ( ESP_SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
-  i2c_master_write_byte(cmd, (HT16K33_BLINK_CMD | HT16K33_BLINK_DISPLAYON | (b << 1)), ACK_CHECK_EN);
-  i2c_master_stop(cmd);
-  esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
-  i2c_cmd_link_delete(cmd);
-  return ret;
-}
-
-static esp_err_t setBrightness(i2c_port_t i2c_num, uint8_t b)
-{
-  if (b > 15) b = 15;
-  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-  i2c_master_start(cmd);
-  i2c_master_write_byte(cmd, ( ESP_SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
-  i2c_master_write_byte(cmd, (HT16K33_CMD_BRIGHTNESS | b), ACK_CHECK_EN);
-  i2c_master_stop(cmd);
-  esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
-  i2c_cmd_link_delete(cmd);
-  return ret;
-}
-
-// equivalent to Adafruit_LEDBackpack::begin
-static esp_err_t begin(i2c_port_t i2c_num)
-{
-  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-  i2c_master_start(cmd);
-  i2c_master_write_byte(cmd, ( ESP_SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
-  i2c_master_write_byte(cmd, 0x21, ACK_CHECK_EN);
-  i2c_master_stop(cmd);
-  esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
-  i2c_cmd_link_delete(cmd);
-  return ret;
-}
-
-// nth: 0-3
-// 0 --- 113
-// 1 --- 115
-// 2 --- 117
-// 3 --- 119
-void setDigit(uint8_t nth, uint8_t num)
-{
-  int index = 0;
-  switch(nth) {
-    case 0:
-      index = 113;
-      break;
-    case 1:
-      index = 115;
-      break;
-    case 2:
-      index = 117;
-      break;
-    case 3:
-      index = 119;
-      break;
-  }
-  data_wr[index] = numbertable[(int)num];
-}
-
-void setTime(uint32_t hour, uint32_t minute) {
-  setDigit(0, hour / 10);
-  setDigit(1, hour % 10);
-  setDigit(2, minute / 10);
-  setDigit(3, minute % 10);
-}
-
-static esp_err_t writeDisplay(i2c_port_t i2c_num, uint8_t* data_wr, size_t size)
-{
-  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-  i2c_master_start(cmd);
-  i2c_master_write_byte(cmd, ( ESP_SLAVE_ADDR << 1 ) , ACK_CHECK_EN);
-  i2c_master_write(cmd, data_wr, size, ACK_CHECK_EN);
-  i2c_master_stop(cmd);
-  esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
-  i2c_cmd_link_delete(cmd);
-  return ret;
 }
 
 
 void app_main()
-{
-    // initialize console and set time variables
-    initialize_nvs();
+{   
 
+    // initialize console
+    initialize_nvs();
     #if CONFIG_STORE_HISTORY
       initialize_filesystem();
     #endif
-
     initialize_console();
-
-    /* Register commands */
     esp_console_register_help_command();
     register_read();
     const char* prompt = LOG_COLOR_I "esp32> " LOG_RESET_COLOR;
-
     int ret;
-
     printf("\n"
-             "This is an example of ESP-IDF console component.\n"
-             "Type 'help' to get the list of commands.\n"
-             "Use UP/DOWN arrows to navigate through command history.\n"
-             "Press TAB when typing command name to auto-complete.\n");
-
-      /* Figure out if the terminal supports escape sequences */
-      int probe_status = linenoiseProbe();
-      if (probe_status) { /* zero indicates success */
+             "Type set with current time (hour, minute, and second) to initialize time for alarm\n");
+    int probe_status = linenoiseProbe();
+    if (probe_status) { 
           printf("\n"
                  "Your terminal application does not support escape sequences.\n"
                  "Line editing and history features are disabled.\n"
                  "On Windows, try using Putty instead.\n");
           linenoiseSetDumbMode(1);
-      #if CONFIG_LOG_COLORS
-              /* Since the terminal doesn't support escape sequences,
-               * don't use color codes in the prompt.
-               */
-              prompt = "esp32> ";
-      #endif //CONFIG_LOG_COLORS
-      }
-
-      //while((hour_count != 99) && (minute_count != 99) && (second_count != 99)){
-        char* line = linenoise(prompt);
-        if (line != NULL) { /* Ignore empty lines */
-          linenoiseHistoryAdd(line);
-        }
-        /* Add the command to the history */
-
-        #if CONFIG_STORE_HISTORY
-          /* Save command history to filesystem */
-          linenoiseHistorySave(HISTORY_PATH);
-        #endif
-
-        /* Try to run the command */
-        esp_err_t err = esp_console_run(line, &ret);
-        if (err == ESP_ERR_NOT_FOUND) {
-          printf("Unrecognized command\n");
-        } else if (err == ESP_ERR_INVALID_ARG) {
-                    // command was empty
-        } else if (err == ESP_OK && ret != ESP_OK) {
-          printf("Command returned non-zero error code: 0x%x (%s)\n", ret, esp_err_to_name(err));
-        } else if (err != ESP_OK) {
-          printf("Internal error: %s\n", esp_err_to_name(err));
-        }
-        linenoiseFree(line);
-      //}
+        #if CONFIG_LOG_COLORS
+                  /* Since the terminal doesn't support escape sequences,
+                   * don't use color codes in the prompt.
+                   */
+                  prompt = "esp32> ";
+        #endif //CONFIG_LOG_COLORS
+    }
+    char* line = linenoise(prompt);
+    if (line != NULL) { 
+        linenoiseHistoryAdd(line);
+    }
+    #if CONFIG_STORE_HISTORY
+        linenoiseHistorySave(HISTORY_PATH);
+    #endif
+    esp_err_t err = esp_console_run(line, &ret);
+    if (err == ESP_ERR_NOT_FOUND) {
+        printf("Unrecognized command\n");
+    } else if (err == ESP_ERR_INVALID_ARG) {
+    } else if (err == ESP_OK && ret != ESP_OK) {
+        printf("Command returned non-zero error code: 0x%x (%s)\n", ret, esp_err_to_name(err));
+    } else if (err != ESP_OK) {
+        printf("Internal error: %s\n", esp_err_to_name(err));
+    }
+    linenoiseFree(line);
+    
 
 
     // initialize i2c communication and alpha display
-    print_mux = xSemaphoreCreateMutex();
-    i2c_example_slave_init();
-    i2c_example_master_init();
-    int size;
-    uint32_t task_idx = 0;
-    uint8_t* data = (uint8_t*) malloc(DATA_LENGTH);
-    data_wr = (uint8_t*) malloc(DATA_LENGTH);
-    for(int i = 0; i < DATA_LENGTH; i++) {
-      data_wr[i] = 0x0;
-    }
-    ret = begin(I2C_EXAMPLE_MASTER_NUM);
-    if(ret == ESP_OK) printf("SETUP OKAY!\n");
-    ret = blinkRate(I2C_EXAMPLE_MASTER_NUM, HT16K33_BLINK_OFF);
-    if(ret == ESP_OK) printf("BLINK RATE SETUP OKAY!\n");
-    ret = setBrightness(I2C_EXAMPLE_MASTER_NUM, 15);
-    if(ret == ESP_OK) printf("BRIGHTNESS SETUP OKAY!\n");
-    xSemaphoreTake(print_mux, portMAX_DELAY);
-    setTime(hour_count, minute_count);
-    ret = writeDisplay( I2C_EXAMPLE_MASTER_NUM, data_wr, RW_TEST_LENGTH);
-    if (ret == ESP_OK) {
-        size = i2c_slave_read_buffer( I2C_EXAMPLE_SLAVE_NUM, data, RW_TEST_LENGTH, 1000 / portTICK_RATE_MS);
-    }
-    xSemaphoreGive(print_mux);
+    init();
+    setTime();
 
     // initialize button gpio input and interrupt handler
     gpio_pad_select_gpio(BTN_GPIO);
@@ -696,71 +509,25 @@ void app_main()
     example_tg1_timer_init(TIMER_0, TEST_WITH_RELOAD, TIMER_INTERVAL1_SEC);
     timer_start(TIMER_GROUP_1, TIMER_0);
     uint32_t current_second = minute_count * 60 + second_count;
-    timer_event_t evt_1;
 
     while (1) {
       if(reset){
         timer_start(TIMER_GROUP_0, TIMER_0);
-        current_second = minute_count * 60 + second_count;
-        while((minute_count * 60 + second_count) <= (current_second + TIMER_INTERVAL0_SEC)){
-          xQueueReceive(timer_queue_1, &evt_1, portMAX_DELAY);
-
-
-          if(second_count > 58) {
-            second_count = 0;
-            if (minute_count > 58) {
-              minute_count = 0;
-              if (hour_count > 22) hour_count = 0;
-              else hour_count ++;
-            } else {
-              minute_count ++;
-            }
-            mcpwm_example_servo_control(0, SECONDHAND_SERVO);
-            mcpwm_example_servo_control(minute_count, MINUTEHAND_SERVO);
-            xSemaphoreTake(print_mux, portMAX_DELAY);
-            setTime(hour_count, minute_count);
-            ret = writeDisplay( I2C_EXAMPLE_MASTER_NUM, data_wr, RW_TEST_LENGTH);
-            if (ret == ESP_OK) {
-                size = i2c_slave_read_buffer( I2C_EXAMPLE_SLAVE_NUM, data, RW_TEST_LENGTH, 1000 / portTICK_RATE_MS);
-            }
-            xSemaphoreGive(print_mux);
-          } else {
-            mcpwm_example_servo_control(second_count, SECONDHAND_SERVO);
-            second_count ++;
-          }
+        current_second = hour_count * 3600 + minute_count * 60 + second_count;
+        while((hour_count * 3600 + minute_count * 60 + second_count) <= (current_second + TIMER_INTERVAL0_SEC)){
+          ticking();
           printf("triggered: %d min : %d s \n", minute_count, second_count);
         }
+
         timer_event_t evt_0;
         xQueueReceive(timer_queue_0, &evt_0, portMAX_DELAY);
+        // called when alarm goes off!
         reset = 0;
-        blink_task(); //when alarm goes off
+        blink_task();
         timer_pause(TIMER_GROUP_0, TIMER_0);
+
       } else {
-        xQueueReceive(timer_queue_1, &evt_1, portMAX_DELAY);
-
-        if(second_count > 58) {
-          second_count = 0;
-          if (minute_count > 58) {
-            minute_count = 0;
-            if (hour_count > 22) hour_count = 0;
-            else hour_count ++;
-          } else {
-            minute_count ++;
-          }
-          mcpwm_example_servo_control(0, SECONDHAND_SERVO);
-          mcpwm_example_servo_control(minute_count, MINUTEHAND_SERVO);
-          xSemaphoreTake(print_mux, portMAX_DELAY);
-          setTime(hour_count, minute_count);
-          ret = writeDisplay( I2C_EXAMPLE_MASTER_NUM, data_wr, RW_TEST_LENGTH);
-          if (ret == ESP_OK) {
-              size = i2c_slave_read_buffer( I2C_EXAMPLE_SLAVE_NUM, data, RW_TEST_LENGTH, 1000 / portTICK_RATE_MS);
-          }
-          xSemaphoreGive(print_mux);
-
-        } else {
-          mcpwm_example_servo_control(second_count, SECONDHAND_SERVO);
-          second_count ++;
-        }
+        ticking();
       }
       printf("%d min : %d s \n", minute_count, second_count);
       vTaskDelay(10);
